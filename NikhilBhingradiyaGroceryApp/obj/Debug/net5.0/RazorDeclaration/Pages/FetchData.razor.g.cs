fb7e6c2af35db6eaa77ca579fd8bcf0bdaf95c23
NikhilBhingradiya <NB76@myscc.ca>
2022-04-13 09:43:13 -0400

    Make Assignment -3 branch
    Make Assignment -3 branch

--- NikhilBhingradiyaGroceryApp/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ NikhilBhingradiyaGroceryApp/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,77 +13,77 @@ namespace NikhilBhingradiyaGroceryApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 1 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 2 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 3 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 4 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 5 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 6 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 7 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 8 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 9 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using NikhilBhingradiyaGroceryApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\_Imports.razor"
+#line 10 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\_Imports.razor"
 using NikhilBhingradiyaGroceryApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\Pages\FetchData.razor"
+#line 3 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\Pages\FetchData.razor"
 using NikhilBhingradiyaGroceryApp.Data;
 
 #line default
@@ -98,7 +98,7 @@ using NikhilBhingradiyaGroceryApp.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "D:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignment\NikhilBhingradiyaGroceryApp\Pages\FetchData.razor"
+#line 39 "d:\sem 2\Asp.net\Nikhil_Bhingradiya_WEB315Assignments\NikhilBhingradiyaGroceryApp\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
